5433e4be0446c11cb36b2572336efe13af5bedff
maripetrova <74290687+maripetrova@users.noreply.github.com>
2022-05-16 12:06:31 +0300

    main page

--- redux-study-proj/src/pages/Main/Main.tsx
+++ redux-study-proj/src/pages/Main/Main.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { useNavigate } from "react-router-dom";
+import '../../App.css'
 
 export function Main() {
-  const navigate = useNavigate();
+    const navigate = useNavigate();
 
     return (
-        <div className={'wrapper-main'}>
-            <button onClick={ () => {navigate('/second')} }>
-              Инструмент №1
-            </button>
+        <div className="container-main">
+            <div className="heading-main">
+                <h1 className="heading-main__title">Лучший инструмент для тайм менеджмента <br/> To-Do List</h1>
+            </div>
+            <button className="button-main"
+                    onClick={() => {
+                        navigate('/second');
+                    }}><span>Инструмент №1</span></button>
+
         </div>
     )
 }
